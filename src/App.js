94150f6dbc1db6b03812e2475d1de40539eacc43
sudhirbarik2 <sudhir.barik981@gmail.com>
2022-02-01 20:31:57 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+import Articles from './Articles';
+
+const title = "Sorting Articles";
+
+function App({ articles }) {
+  const [flag, setFlag] = useState(false)
+  const sortByUpvotes = () => {
+    let arr = (articles.sort(function (a, b) {
+      return a.upvotes > b.upvotes ? -1 : b.upvotes > a.upvotes ? 1 : 0
+    }))
+    Array.prototype.reverse(arr)
+    return arr
+  }
+
+  const sortByDates = () => {
+    return (articles.sort(function (a, b) {
+      return new Date(a.date) > new Date(b.date) ? -1 : new Date(b.date) > new Date(a.date) ? 1 : 0;
+    }));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="layout-row align-items-center justify-content-center my-20 navigation">
+        <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+        <button data-testid="most-upvoted-link" onClick={() => setFlag(false)} className="small">Most Upvoted</button>
+        <button data-testid="most-recent-link" onClick={() => setFlag(true)} className="small">Most Recent</button>
+      </div>
+      <Articles articles={flag ? sortByDates() : sortByUpvotes()} />
     </div>
   );
+
 }
 
 export default App;
